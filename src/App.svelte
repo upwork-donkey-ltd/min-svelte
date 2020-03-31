d945703c66d3fff302e337b4bf50e9925cb1d560
KVexcavator <str_token@mail.ru>
2020-03-31 15:12:35 +0300

    add reactive

--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,34 @@
 <script>
 	let name = 'Svelte';
-  let inputValue = "";
-  let pos = {
-    x: 0,
-    y: 0
+  let counter = 0;
+  $: counterClass = counter % 2===0 ?"red":"blue";
+  $: upperName = name.toUpperCase();
+  $: lowerName = name.toLowerCase();
+  $: {
+    console.log("Name: ", name);
+    console.log("Counter: ", counter);
   }
-  function changeNameHandler(){
-    name = "This name changed!";
+  $: if (counter === 10){
+    name = "Counter is equal 10"
   }
-  function mousemoveHandler(event){
-    pos.x = event.x;
-    pos.y = event.y;
-  }
-  function submitHandler(event){
-    // event.preventDefault();
-    console.log("inputvalue: ", inputValue);
+  function changeName(){
+    name = "New name";
   }
 </script>
 
 <h1>{name}</h1>
-<button on:click={changeNameHandler}>Change name</button>
-<div class="playground" on:mousemove={mousemoveHandler}>
-  <h2>X: {pos.x}, Y: {pos.y}</h2>
-</div>
-<form on:submit|preventDefault|once={submitHandler}>
-  <input type="text" on:input={event =>(inputValue = event.target.value)} />
-  <button type="submit">Submit form</button>
-</form>
+<h2>{upperName}</h2>
+<h2>{lowerName}</h2>
+<button on:click={changeName}>Change name</button>
+
+<h2 class={counterClass} >Counter {counter}</h2>
+<button on:click={()=>counter++}>Add 1 to counter</button>
 
 <style>
-	:global(h1) {
-		color: purple;
-	}
-  .playground {
-    width: 400px;
-    height: 200px;
-    padding:1rem;
-    margin-bottom: 1rem;
-    border: 1px solid black;
-  }
+ .blue{
+  color: blue;
+ }
+ .red{
+  color: red;
+ }
 </style>
