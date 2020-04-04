dd3a051dc776021f8f25f72c1f4c572dcc649d91
KVexcavator <str_token@mail.ru>
2020-04-04 19:10:44 +0300

    how work cicles

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,43 @@
 <script>
   import Person from './Person.svelte';
-  const people = [
-    {id: 1, name: "Goddess", age: 100, job: "Seniorita"},
-    {id: 2, name: "Asshole", age: 15, job: "Conjier"}
-  ]
 
-  let name="Barmaley"
-  let age = 20
-  let jobTitle = "ZombiKiller"
+  let value = "";
+  let people = [
+    {id: 1, name: "Goddess"},
+    {id: 2, name: "Asshole"},
+    {id: 3, name: "Barmaley"}
+  ];
+
+  function removeFirst(){
+    people = people.slice(1);
+  }
+
+  function addPerson(){
+    // people = people.concat([
+    //  {
+    //    id: Date.now(),
+    //    name: value
+    //  }
+    //]);
+    people = [...people, {
+      id: Date.now(),
+      name: value
+    }]
+    value = "";
+  }
 </script>
 
-<h1>Application</h1>
+<input type="text" bind:value />
+<button on:click={addPerson}>Add Person</button>
+<button on:click={removeFirst}>Remove First</button>
 
-<Person name={people[0].name} age={people[0].age} job={people[0].job} />
-<Person {...people[1]} />
-<Person {name} {age} job={jobTitle} />
+<hr>
+
+{#each people as {name, id}, i (id)}
+  <Person {name} index={i}/>
+{:else}
+  <p>No people</p>
+{/each}
 
 <style>
 </style>
--- src/Person.svelte
+++ src/Person.svelte
@@ -1,24 +1,11 @@
 <script>
   export let name = "Undefied Name"
-  export let age = "Undefined Age"
-  export let job = "Undefined Job"
+  export let index;
+
+  let copy = name;
 </script>
 
-<div>
-  <p>Имя: <b>{name}</b></p>
-  <p>Вид деятельности: <b>{job}</b></p>
-
-  {#if age < 18}
-    <p>Спиртное не продавать!</p>
-  {:else}
-    <p>Делай что хочешь!</p>
-  {/if}
-</div>
+<p><strong>{index+1}: </strong>{name} / {copy}</p>
 
 <style>
-  div {
-    border: 1px solid black;
-    padding: 1rem;
-    margin-bottom: 1rem;
-  }
 </style>
