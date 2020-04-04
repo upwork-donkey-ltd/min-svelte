b688327693ad6e1f777f1a367ee494e228e348de
KVexcavator <str_token@mail.ru>
2020-04-04 17:01:49 +0300

    conditionaly operators (if else)

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,31 @@
 <script>
-  let name = 'Svelte';
-  let agree = true;
-  let text = "";
-  let select = "1";
-  let sex = "male";
+  let value = "Hello"
+
+  $: error = !isValid(value)
+  function isValid(v){
+    return v.length > 5 && v.length <10
+  }
 </script>
 
-<h1>{name}</h1>
-<input type="text" bind:value={name} />
-<hr>
-<input type="checkbox" bind:checked={agree} />
-{agree}
-<hr>
-<textarea bind:value={text} />
-<div style="white-space: pre-wrap">{text}</div>
-<hr>
-<select bind:value={select}>
-  <option value="0">Option 0</option>
-  <option value="1">Option 1</option>
-  <option value="2">Option 2</option>
-</select>
-<hr>
-<input type="radio" value="female" bind:group={sex}/>Female
-<input type="radio" value="male" bind:group={sex}/>Male
-<br />
-Sex: {sex}
-<hr>
+<h1>Application</h1>
+<input type="text" bind:value={value} class:red={error} class:green={!error}/>
+
+{#if value.length < 5}
+  <p>Длина меньше 5 символов</p>
+{:else if value.length > 10}
+  <p>Длина больше 10 символов</p>
+{:else}
+  <p>Длина между 5 и 10</p>
+{/if}
 
 <style>
+  input {
+    outline: none;
+  }
+  .red {
+    border-color: red;
+  }
+  .green {
+    border-color: green;
+  }
 </style>
